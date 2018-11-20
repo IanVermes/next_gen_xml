578785ea9f3849cda9e0e2b4bb5e661e894af0ba
Ian Vermes <ian.vermes@gmail.com>
2018-11-20 17:07:27 +0000

    Imports work on program/integration/unittest level; tests pass; core.py sets settings singelton.

--- validation/tests/test_integration.py
+++ validation/tests/test_integration.py
@@ -38,19 +38,65 @@ class Integration_CommandLine_Entry(CommandLineTestCase):
         assert os.path.isfile(self.entry_point_py)
 
         self.error_text = f"{os.path.basename(self.entry_point_py)}: error"
+        self.unformatted_cmd = "python {} {}"
 
-    def test_user_expected_arg(self):
+    def invoke_cmd_via_commandline(self, unformatted_cmd, expected_status):
+        """Collects the stdout when invoking and validates the exit status."""
         quote = shlex.quote
-        cmd = "python {} {}".format(quote(self.entry_point_py),
-                                    quote(self.dir_valid))
+        cmd = unformatted_cmd.format(quote(self.entry_point_py),
+                                     quote(self.dir_valid))
 
-        # User invokes the script through the commandline with a directory
         status, stdout = subprocess.getstatusoutput(cmd)
 
-        # Scipt processes user input exiting successfully.
-        self.assertEqual(status, 0)
-        self.assertNotIn(self.error_text, stdout.lower())
-        self.assertIn("done!", stdout.lower())
+        if expected_status == 0:
+            self.assertEqual(status, 0,
+                             msg=f"'$ {cmd}': did not exit cleanly/validly.")
+        elif 0 < expected_status < 3:
+            self.assertGreater(status, 0,
+                               msg=(f"'$ {cmd}': should have exited with an "
+                                    "error but exited cleanly."))
+        else:
+            msg = (f"Got expected_status={repr(expected_status)}, "
+                   "not int: 0 <= i < 3.")
+            raise ValueError(msg)
+
+        return stdout
+
+    def test_user_expected_arg(self):
+        # User invokes the script through the commandline with a directory
+        cmd = self.unformatted_cmd
+
+        # Script processes user input while printing to the CLI.
+        status = 0
+        stdout = self.invoke_cmd_via_commandline(cmd, expected_status=status)
+        stdout = stdout.lower()
+
+        # User reads CLI to confirm details.
+        self.assertNotIn(self.error_text, stdout)
+        self.assertNotIn("mode.test", stdout)
+        self.assertIn("mode.live", stdout)
+        self.assertIn("done!", stdout)
+
+    def test_user_expected_arg_with_optional_arg(self):
+        rawcmd = self.unformatted_cmd + " "
+        options = ["-t", "--test"]
+        cmds = {opt: rawcmd + opt for opt in options}
+
+        # User invokes the script through the commandline with a
+        # directory and optional argument
+        status = 0
+        for option, cmd in cmds.items():
+            with self.subTest(optional_arg=option):
+                args = cmd, status
+
+                # Script processes user input while printing to the CLI.
+                stdout = self.invoke_cmd_via_commandline(*args)
+                stdout = stdout.lower()
+
+                # User reads CLI to confirm details.
+                self.assertNotIn(self.error_text, stdout)
+                self.assertIn("done!", stdout)
+                self.assertIn("mode.test", stdout)
 
     def test_user_no_arg(self):
         quote = shlex.quote
@@ -66,8 +112,9 @@ class Integration_CommandLine_Entry(CommandLineTestCase):
 
     def test_user_invalid_dir_arg(self):
         quote = shlex.quote
-        cmd = "python {} {}".format(quote(self.entry_point_py),
-                                    quote(self.dir_invalid))
+        cmd = self.unformatted_cmd
+        cmd = cmd.format(quote(self.entry_point_py),
+                         quote(self.dir_invalid))
 
         # User invokes the script through the commandline WITHOUT a directory
         status, stdout = subprocess.getstatusoutput(cmd)
--- validation/validator/core.py
+++ validation/validator/core.py
@@ -14,8 +14,8 @@ import helpers
 
 import os
 
-CORE_SETTINGS_FILENAME = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), "CORE_SETTINGS.ini"))
+CORE_SETTINGS_FILENAME = os.path.join(os.path.dirname(__file__),
+                                      "CORE_SETTINGS.ini")
 
 
 class _TestingPrimitive():
@@ -33,20 +33,30 @@ class _TestingPrimitive():
         raise package_base_eror
 
 
-def main(directory):
+def main(directory, testmode=False):
     """Validate the XML in the directory and reporting on each."""
-    # Read the ini file for log file default name
-    # Infer the log file output location from the given directory
+    # Set the mode depending on the main Kwargs.
+    if testmode is True:
+        mode = helpers.settings_handler.Mode.TEST
+    elif testmode is False:
+        mode = helpers.settings_handler.Mode.LIVE
+    # Get the ini file, from an expected location OR failing that find it.
+    ini_file = helpers.path.expandpath(CORE_SETTINGS_FILENAME, exists=True)
+    settings = helpers.settings_handler.Settings(ini_file, mode=mode)
+
+    # Create a settings singleton from the ini file.
+    # Infer the log file output location from the given directory. # HUH WHAT DID I MEAN?
     # Generate a list of xml file paths to iterate over
     # Feed errors to an error parser that works with the log file
     # Parse the xml with lxml and the XSD
     # Perform examinations that are beyond the scope of XSD
 
-    print("done!")
+    print(f"test mode: {settings.mode}")  # TODO remove one your integration tests are more fully written
+    print("done!") # TODO remove one your integration tests are more fully written
     return
 
 
 if __name__ == '__main__':
     argparser = helpers.argparse.NextGenArgParse()
     args = argparser.get_args()
-    main(args.directory)
+    main(args.directory, testmode=args.testmode)
