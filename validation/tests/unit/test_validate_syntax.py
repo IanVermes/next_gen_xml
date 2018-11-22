8ae08c803ad7627b59572da3a8fbf073882fed47
Ian Vermes <ian.vermes@gmail.com>
2018-11-22 14:23:21 +0000

    Developing xml.validate_syntax()

--- validation/tests/unit/test_validate_syntax.py
+++ validation/tests/unit/test_validate_syntax.py
@@ -7,17 +7,38 @@ Copyright Ian Vermes 2018
 """
 
 from tests.base_testcases import XMLValidationTestCase
+from helpers.xml import validate_syntax
+import exceptions
 
 import os
 import unittest
+import functools
 
 
 class TestSyntaxValidation(XMLValidationTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.base_exc = exceptions.NextGenError
+
+    def _validator(self, *args, _func):
+        """Basis for partialmethod"""
+        return _func(*args)
+
+    validator = functools.partialmethod(_validator, _func=validate_syntax)
 
     @unittest.expectedFailure
     def test_fail(self):
         self.fail("Not written.")
 
+    def test_nofalses_after_syntax_validation(self):
+
+        kwargs = {"values": self.files,
+                  "criterion": "syntax",
+                  "validator": self.validator,
+                  "permitted_exceptions": self.base_exc}
+        self.assertNoFalsePositivesOrNegatives(**kwargs)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ validation/validator/helpers/xml.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A collection of XML functions and classes that validate xml.
+
+Copyright Ian Vermes 2018
+"""
+
+
+def validate_syntax(filename):
+    return bool(filename)
