943fc3f60f942283ee4f35144f7af7061e642f78
Ian Vermes <ian.vermes@gmail.com>
2019-04-24 17:02:49 +0100

    Removed: All test code related to bad testcase code.
    The testing code from in tests.base_testcases.XMLValidation is messy, 
    poorly organised and complicated but more importantly it fails to direct 
    code development nor assists with correctly verifying the validation 
    functions like validate_syntax/validate_schema correctly function. A 
    thoroughly new testing suit is needed for these functions and a 
    reorganisation of the resource files.

--- validation/tests/unit/test_validate_schema.py
+++ validation/tests/unit/test_validate_schema.py
@@ -6,7 +6,7 @@
 Copyright Ian Vermes 2018
 """
 
-from tests.base_testcases import XMLValidation, ExtendedTestCase
+from tests.base_testcases import ExtendedTestCase
 from helpers.checkschema import SchemaOperations, validate_schema
 from helpers.enum import EncodingErrorCode
 from helpers.result import ValidationResult
@@ -47,24 +47,6 @@ class TestSchemaDependency(ExtendedTestCase):
         module = helpers.checkschema
 
         self.assertHasAttr(module, expected_attr)
-
-
-class TestSchemaValidation(XMLValidation.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.criterion = criterion = "schema"
-        cls.base_exc = exceptions.SchemaValidationError
-        cls.valid_file = next(cls.get_resources_by_criterion("valid"))
-        cls.illegal_file = next(cls.get_resources_by_criterion(criterion))
-        cls.result_type = ValidationResult
-        cls.cause_exc = etree.DocumentInvalid
-        cls.imported_validation_func = (validate_schema, )
-
-    def test_validation_func_return_type(self):
-        self.fail("not written")
-
 
 if __name__ == '__main__':
     unittest.main()
--- validation/tests/unit/test_validate_syntax.py
+++ validation/tests/unit/test_validate_syntax.py
@@ -6,36 +6,15 @@
 Copyright Ian Vermes 2018
 """
 
-from tests.base_testcases import XMLValidation
-from helpers.xml import validate_syntax, ValidationResult
+# from tests.base_testcases import XMLValidation
+from helpers.result import ValidationResult
+from helpers.checkencoding import validate_syntax
 import exceptions
 
 from lxml import etree
 
 import os
 import unittest
-
-
-class TestSyntaxValidation(XMLValidation.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.criterion = criterion = "syntax"
-        cls.base_exc = exceptions.SyntaxValidationError
-        cls.cause_exc = etree.XMLSyntaxError
-        cls.valid_file = next(cls.get_resources_by_criterion("valid"))
-        cls.illegal_file = next(cls.get_resources_by_criterion(criterion))
-        cls.result_type = ValidationResult
-        cls.imported_validation_func = (validate_syntax, )
-
-    def test_nofalses_after_syntax_validation(self):
-
-        kwargs = {"values": self.files,
-                  "criterion": self.criterion,
-                  "validator": self.validator,
-                  "permitted_exceptions": self.base_exc}
-        self.assertNoFalsePositivesOrNegatives(**kwargs)
 
 
 if __name__ == '__main__':
