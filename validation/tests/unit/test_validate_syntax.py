e5969c5993abb7398f653303f404deb8673ff80d
Ian Vermes <ian.vermes@gmail.com>
2019-05-08 13:13:11 +0100

    Added: ValidationTestCase generic tests against syntax XML resources

--- validation/tests/unit/test_validate_syntax.py
+++ validation/tests/unit/test_validate_syntax.py
@@ -5,17 +5,31 @@
 
 Copyright Ian Vermes 2018
 """
-
-# from tests.base_testcases import XMLValidation
-from helpers.result import ValidationResult
+from tests.base_testcases import ExtendedTestCase
+from tests.validation_testcases import ValidationTestCase
 from helpers.checkencoding import validate_syntax
+from helpers.enum import Passing
+from helpers.result import ValidationResult
 import exceptions
 
 from lxml import etree
 
-import os
 import unittest
 
+
+# @unittest.skip("skip")
+class TestValidateSchemaFunction(ValidationTestCase, ExtendedTestCase):
+    """Some test methods are written in the first parent class
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        resource = "tests/resources/syntax"
+        func = validate_syntax
+        failing_enum = Passing.SYNTAX
+
+        cls.preSetup(directory=resource, validator=func, enum=failing_enum)
+        cls.resource_dict = cls.get_resources()
 
 if __name__ == '__main__':
     unittest.main()
