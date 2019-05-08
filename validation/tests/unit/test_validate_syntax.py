7ab9fd9f316d727a105f02ecb0f466ce462e63a5
Ian Vermes <ian.vermes@gmail.com>
2019-05-08 14:28:37 +0100

    Extended: tests for handling false positive syntax cases.
    handle_encoding_errors()/raise_if_mismatched_encodings do very similar 
    thing. The test for raise_if_mismatched_encodings is to confirm that 
    encoding mismatches raise the package error when given a failing XML 
    file.

--- validation/tests/unit/test_validate_syntax.py
+++ validation/tests/unit/test_validate_syntax.py
@@ -7,7 +7,7 @@ Copyright Ian Vermes 2018
 """
 from tests.base_testcases import ExtendedTestCase
 from tests.validation_testcases import ValidationTestCase
-from helpers.checkencoding import validate_syntax
+from helpers.checkencoding import validate_syntax, raise_if_mismatched_encodings
 from helpers.enum import Passing
 from helpers.result import ValidationResult
 import exceptions
@@ -29,6 +29,40 @@ class TestValidateSchemaFunction(ValidationTestCase, ExtendedTestCase):
 
         cls.preSetup(directory=resource, validator=func, enum=failing_enum)
         cls.resource_dict = cls.get_resources()
+
+    def test_encoding_mismatch_func(self):
+        """The encoding mismatch function is used by validate_syntax."""
+        func = raise_if_mismatched_encodings
+        expected_exception = exceptions.EncodingOperationError
+        true_positives = self.resource_dict[True]
+        true_negatives = ["illegal_syntax_declaration_encoding_value_4.xml",
+                          "illegal_syntax_declaration_encoding_mismatch_4.xml"]
+        true_negatives = [self.failing_directory.joinpath(f) for f in true_negatives]
+
+        for filename in true_positives:
+            with self.subTest(true_pos_filename=filename):
+                try:
+                    func(filename)
+                except expected_exception as unexpected:
+                    # False negative!
+                    msg = ("Got a false negative - the function raised "
+                           f"{expected_exception} for a case it should not "
+                           "have.")
+                    raise AssertionError(msg) from unexpected
+                except Exception as unexpected:
+                    raise AssertionError() from unexpected
+                else:
+                    pass
+
+        for filename in true_negatives:
+            with self.subTest(true_neg_filename=filename):
+                msg = ("Got a false positive - the case should have raised an "
+                       "error but did not.")
+                with self.assertRaises(Exception, msg=msg) as error:
+                    func(filename)
+                msg = ("Got a false negative - wrong exception was raised.")
+                self.assertIsInstance(error.exception, expected_exception, msg=msg)
+
 
 if __name__ == '__main__':
     unittest.main()
